fba63ad72e4d49cde61d97bff77279df498c9bee
Serge Ilyashenko <age_bookend_0j@icloud.com>
2025-08-29 14:35:29 +0200

    add morgan logging

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import hbs from 'hbs';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(morgan('dev'));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
