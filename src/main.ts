724243ec02f890405deba3af60baaed4837cbf45
Serge Ilyashenko <age_bookend_0j@icloud.com>
2025-09-05 12:30:59 +0200

    fix(main): fix production build

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { join } from 'path';
 import hbs from 'hbs';
+import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
