3a56fd2eecaab8efbfae2dc1e971eb12d2397d3c
Serge Ilyashenko <age_bookend_0j@icloud.com>
2025-09-07 13:39:08 +0200

    feat(joke.mapper): use manual mapping

--- src/domain/joke/mappers/joke.mapper.ts
+++ src/domain/joke/mappers/joke.mapper.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Joke } from '../entities/joke.entity';
 import { JokeApiDto } from '../dto/joke-api.dto';
 import { JokeViewDto } from '../dto/joke-view.dto';
-import { CreateJokeDto } from '../dto/create-joke.dto';
 
 @Injectable()
 export class JokeMapper {
@@ -10,30 +9,27 @@ export class JokeMapper {
 
   toApiDto(entities: Joke[]): JokeApiDto[];
   toApiDto(entity: Joke): JokeApiDto;
-  toApiDto(input: Joke | Joke[]) {
+  toApiDto(input: Joke | Joke[]): JokeApiDto | JokeApiDto[] {
     if (Array.isArray(input)) {
       return input.map((entity) => this.toApiDto(entity));
     }
-    const apiDto = new JokeApiDto();
-    apiDto.id = input.id;
-    apiDto.content = input.content;
-    apiDto.createdAt = input.createdAt;
-    return apiDto;
+    return {
+      id: input.id,
+      content: input.content,
+      createdAt: input.createdAt,
+    };
   }
 
   toViewDto(entities: Joke[]): JokeViewDto[];
   toViewDto(entity: Joke): JokeViewDto;
-  toViewDto(input: Joke | Joke[]) {
+  toViewDto(input: Joke | Joke[]): JokeViewDto | JokeViewDto[] {
     if (Array.isArray(input)) {
       return input.map((entity) => this.toViewDto(entity));
     }
-    // FIXME: Replace with automapper mapper
-    const viewDto = new JokeViewDto();
-    viewDto.id = input.id;
-    viewDto.content = input.content;
-    viewDto.createdAt = input.createdAt.toLocaleDateString('en-SE');
-    return viewDto;
+    return {
+      id: input.id,
+      content: input.content,
+      createdAt: input.createdAt.toLocaleDateString('en-SE'),
+    };
   }
-
-  fromCreateDto(dto: CreateJokeDto) {}
 }
