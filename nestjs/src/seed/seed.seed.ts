8889c8ee13e93c48b415a8d9fb2e2d6f9ea3eaa5
Siarhei Ilyashenka <age_bookend_0j@icloud.com>
2025-12-30 22:46:42 +0100

    feat(nestjs): seed password hash

--- nestjs/src/seed/seed.seed.ts
+++ nestjs/src/seed/seed.seed.ts
@@ -4,7 +4,8 @@ import { seedUsers } from './users.seed';
 const users = [
   {
     username: 'testuser1',
-    passwordHash: 'hashedpassword',
+    passwordHash:
+      '$2a$12$qmf.Kle8.eAvQghiIiuTWOH2ohiKqDVvZYHthmKwGKGYuwfiy2/I6',
     jokes: [
       'What do you call corn that joins the army? Kernel.',
       'Did you know Albert Einstein was a real person? All this time, I thought he was just a theoretical physicist!',
@@ -12,7 +13,8 @@ const users = [
   },
   {
     username: 'solidsnake',
-    passwordHash: 'hashedpassword',
+    passwordHash:
+      '$2a$12$jjR8JMIWph6sTsnpZ7CPyu48C1ODiH6c/zqt1dOJPKBekTdBOV7c6',
     jokes: [
       'What was a more important invention than the first telephone? The second one.',
       "Ben & Jerry's really need to improve their operation. The only way to get there is down a rocky road.",
